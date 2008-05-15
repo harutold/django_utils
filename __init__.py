c5c3c241ddd56c6990d6bc291b6f9813de26bcad
innovateit_barbuza <innovateit_barbuza@a6f2dbe3-4e0d-4881-b4a4-91aefe8467d7>
2008-05-15 07:20:18 +0000

    profiling support
    git-svn-id: http://sociallab.unfuddle.com/svn/sociallab_sandbox/django-addons/django_utils@15 a6f2dbe3-4e0d-4881-b4a4-91aefe8467d7

--- __init__.py
+++ __init__.py
@@ -3,6 +3,7 @@ from cache import *
 from decorators import *
 from functions import *
 from email_auth_backend import *
+from profiling_middleware import *
 
 # native django imports
 from django.http import HttpResponse, Http404, HttpResponseRedirect
