efe11c36444910da855482279a4d71721997bda2
innovateit_barbuza <innovateit_barbuza@a6f2dbe3-4e0d-4881-b4a4-91aefe8467d7>
2008-05-19 09:44:17 +0000

    profiling middlewre py2.4 incompatible
    git-svn-id: http://sociallab.unfuddle.com/svn/sociallab_sandbox/django-addons/django_utils@16 a6f2dbe3-4e0d-4881-b4a4-91aefe8467d7

--- __init__.py
+++ __init__.py
@@ -3,7 +3,6 @@ from cache import *
 from decorators import *
 from functions import *
 from email_auth_backend import *
-from profiling_middleware import *
 
 # native django imports
 from django.http import HttpResponse, Http404, HttpResponseRedirect
