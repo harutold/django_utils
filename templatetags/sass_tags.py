dc11d0bedc21575866748fdb084fa4dd6c9242ee
fhen <fhen@fh-new.(none)>
2009-12-11 01:02:33 +0300

    Экспериментальный темплейттег для sass

---
+++ templatetags/sass_tags.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import os
+from warnings import warn
+
+from django.template import Library, Node, TemplateSyntaxError
+from django.conf import settings
+
+sass_dir = getattr(settings, 'SASS_DIR', 'media/css/')
+sass_path = os.path.join(settings.CURDIR, sass_dir)
+register = Library()
+
+@register.simple_tag
+def sass_to_string(arg):
+    import sass # Пока тут
+    path = os.path.join(sass_path, arg)
+    if not os.path.isfile(path):
+        raise TemplateSyntaxError(u'Sass file %s does not exist' % path)
+    f = open(path)
+    content = f.read()
+    f.close()
+    
+    try:
+        css = sass.convert(content)
+        css = u'<style>/*SASS file %s*/ \n %s</style>' % (arg, css)
+    except:
+        warn('unable to parse file %s' % arg)
+        css = os.system('sass %s %s.css' % (path, path))
+        cssdir = os.path.join(sass_dir, arg)
+        css = u'<link rel="stylesheet" href="/%s.css" type="text/css"></style>' % cssdir
+    return css
+    
